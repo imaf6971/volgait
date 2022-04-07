ebc2061817cbf71eda363a04d836b317c3ebbabf
Elvie Rayl <imaf6971@gmail.com>
2022-04-07 21:00:59 +0300

    user dto now final

--- src/main/java/ru/tisbi/volgait/registration/domain/UserDto.java
+++ src/main/java/ru/tisbi/volgait/registration/domain/UserDto.java
@@ -1,27 +1,60 @@
 package ru.tisbi.volgait.registration.domain;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.Getter;
 
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
+import java.util.Objects;
 
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
 public class UserDto {
     @Email(message = "Введите валидный email!")
     @NotBlank(message = "Email не должен быть пустым!")
-    private String email;
+    private final String email;
 
     @NotBlank(message = "Пароль не должен быть пустым!")
-    private String password;
+    private final String password;
 
     @NotBlank(message = "Пароль не должен быть пустым!")
-    private String matchingPassword;
+    private final String matchingPassword;
+
+    public UserDto() {
+        email = "";
+        password = "";
+        matchingPassword = "";
+    }
+
+    public UserDto(String email, String password, String matchingPassword) {
+        this.email = email;
+        this.password = password;
+        this.matchingPassword = matchingPassword;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getMatchingPassword() {
+        return matchingPassword;
+    }
 
     public boolean isPasswordsMatch() {
         return password.equals(matchingPassword);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        UserDto userDto = (UserDto) o;
+        return Objects.equals(email, userDto.email) && Objects.equals(password, userDto.password) && Objects.equals(matchingPassword, userDto.matchingPassword);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(email, password, matchingPassword);
     }
 }
