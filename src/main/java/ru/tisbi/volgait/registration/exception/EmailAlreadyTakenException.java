0b04fc01d46cd2f88ccfe4e91ea418e50de71159
Elvie Rayl <imaf6971@gmail.com>
2022-04-07 17:44:53 +0300

    simplified exceptions

--- src/main/java/ru/tisbi/volgait/registration/exception/EmailAlreadyTakenException.java
+++ src/main/java/ru/tisbi/volgait/registration/exception/EmailAlreadyTakenException.java
@@ -9,24 +9,9 @@ public class EmailAlreadyTakenException extends RuntimeException {
         super(arg0);
     }
 
-    public EmailAlreadyTakenException(Throwable arg0) {
-        super(arg0);
-    }
-
-    public EmailAlreadyTakenException(String arg0, Throwable arg1) {
-        super(arg0, arg1);
-    }
-
-    public EmailAlreadyTakenException(String arg0, Throwable arg1, boolean arg2, boolean arg3) {
-        super(arg0, arg1, arg2, arg3);
-    }
-
     @Override
     public boolean equals(Object arg0) {
-        if (arg0 instanceof EmailAlreadyTakenException) {
-            return true;
-        }
-        return false;
+        return arg0 instanceof EmailAlreadyTakenException;
     }
     
 }
