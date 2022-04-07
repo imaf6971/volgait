48d9b0cd5968b02d05bbc45e9f672d0f624facc8
Elvie Rayl <imaf6971@gmail.com>
2022-04-07 17:50:01 +0300

    separated UserDetailsServiceImpl

---
+++ src/main/java/ru/tisbi/volgait/auth/service/UserDetailsServiceImpl.java
@@ -0,0 +1,27 @@
+package ru.tisbi.volgait.auth.service;
+
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import ru.tisbi.volgait.registration.repository.UserRepository;
+
+import java.util.ArrayList;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    public UserDetailsServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        var user = userRepository.findByEmail(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User " + username + " not found!"));
+        return new User(user.getEmail(), user.getPassword(), new ArrayList<>());
+    }
+}
--- src/main/java/ru/tisbi/volgait/registration/service/impl/UserServiceImpl.java
+++ src/main/java/ru/tisbi/volgait/registration/service/impl/UserServiceImpl.java
@@ -1,25 +1,19 @@
 package ru.tisbi.volgait.registration.service.impl;
 
-import java.util.ArrayList;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import lombok.extern.slf4j.Slf4j;
-import ru.tisbi.volgait.controller.UserDto;
+import ru.tisbi.volgait.registration.domain.UserDto;
 import ru.tisbi.volgait.registration.domain.User;
 import ru.tisbi.volgait.registration.exception.EmailAlreadyTakenException;
 import ru.tisbi.volgait.registration.exception.PasswordsDoesntMatchException;
 import ru.tisbi.volgait.registration.repository.UserRepository;
 import ru.tisbi.volgait.registration.service.UserService;
 
-import static org.springframework.security.core.userdetails.User.withUsername;
-
 @Slf4j
 @Service
-public class UserServiceImpl implements UserService, UserDetailsService {
+public class UserServiceImpl implements UserService {
 
     private final UserRepository userRepository;
 
@@ -32,7 +26,7 @@ public class UserServiceImpl implements UserService, UserDetailsService {
 
     @Override
     public void register(UserDto user) {
-        if (user.isPasswordsMatch() == false) {
+        if (!user.isPasswordsMatch()) {
             throw new PasswordsDoesntMatchException("Passwords doesnt match!");
         }
         if (emailAlreadyTaken(user.getEmail())) {
@@ -48,21 +42,7 @@ public class UserServiceImpl implements UserService, UserDetailsService {
     private void registerNewUser(UserDto userDto) {
         User user = new User();
         user.setEmail(userDto.getEmail());
-        user.setPassword(userDto.getPassword());
+        user.setPassword(passwordEncoder.encode(userDto.getPassword()));
         userRepository.save(user);
     }
-
-    @Override
-    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        log.info("searching user {}", username);
-        User user = userRepository.findByEmail(username)
-                .orElseThrow(
-                        () -> new UsernameNotFoundException("User " + username + " not found!"));
-        return withUsername(user.getEmail())
-                .password(user.getPassword())
-                .passwordEncoder(passwordEncoder::encode)
-                .authorities(new ArrayList<>())
-                .build();
-    }
-
 }
