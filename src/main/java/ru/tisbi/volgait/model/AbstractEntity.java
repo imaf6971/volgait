147fafc517bbb44f6ebbf0fca146b6ebb82ae4b6
imaf6971 <imaf6971@gmail.com>
2022-04-28 08:26:30 +0300

    Added abstract entity to model

---
+++ src/main/java/ru/tisbi/volgait/model/AbstractEntity.java
@@ -0,0 +1,25 @@
+package ru.tisbi.volgait.model;
+
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
+
+@MappedSuperclass
+public abstract class AbstractEntity {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE)
+	@Column(name = "id", unique = true, nullable = false)
+	private Long id;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+	
+}
